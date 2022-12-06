4b6e48af947f515c66193b6154be9374e519f4c7
caiqupedasilva <caiquemaverik2@gmail.com>
2022-12-06 12:33:11 -0300

    Finalizando projeto

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
-
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
 
-    listaDeTeclas[contador].onclick = function () {
+    tecla.onclick = function () {
         tocaSom(idAudio);
-    };
+    }   
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
+        console.log(evento.code)
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }                                  
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
+
+
+// let contador = 0;
+
+// while (contador < listaDeTeclas.length) {
+
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+
+//     const idAudio = `#som_${instrumento}`;
+
+//     listaDeTeclas[contador].onclick = function () {
+//         tocaSom(idAudio);
+//     };
+
+//     contador = contador + 1;
+// }
